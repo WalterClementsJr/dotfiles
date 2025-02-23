fd4a66ff7b160e110195fe12e970e282cbe28430
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2025-02-23 18:48:59 +0700

    feat: update nvim conf

--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,11 @@ return {
     "saghen/blink.cmp",
     enabled = false,
   },
+  {
+    -- disable the clock https://www.lazyvim.org/plugins/ui#lualinenvim
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.sections.lualine_z = {}
+    end,
+  },
 }
