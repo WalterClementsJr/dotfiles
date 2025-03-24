ad5f5eda918b4730a68878b132ad35acd5c55cc6
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2025-03-25 05:14:45 +0700

    feat: nvim undo plugin

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,18 @@
-return {}
--- return {
---   "nvim-telescope/telescope.nvim",
---   keys = {
---     -- adding keymaps for live_grep
---     {},
---   },
--- }
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "debugloop/telescope-undo.nvim",
+    },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          undo = {},
+        },
+      })
+      require("telescope").load_extension("undo")
+      vim.keymap.set("n", "<leader>uu", "<cmd>Telescope file history<cr>")
+    end,
+  },
+}
