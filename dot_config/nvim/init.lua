d90704f093f0cfe7ea2561a6f499c3c9568a7211
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2025-08-05 12:01:01 +0700

    feat: nvim transparent background

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,4 +1,2 @@
 require("config.lazy")
 require("outline").setup({})
-
-vim.cmd("colorscheme moonfly")
--- dot_config/nvim/lua/plugins/main.lua
+++ dot_config/nvim/lua/plugins/main.lua
@@ -1,5 +1,14 @@
 return {
-  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+  {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.moonflyTransparent = true
+      vim.cmd("colorscheme moonfly")
+    end,
+  },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
