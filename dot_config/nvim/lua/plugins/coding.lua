95be3be31b3866720080b17cbb2b8b19ded44d44
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2025-10-13 23:14:49 +0700

    chore/test: nvim features
    feat: nvim theme
    
    feat: nvim feature config
    
    - lsp
    
    - formatter
    
    - display outline

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -1,25 +1,15 @@
 return {
   {
-    "mason-org/mason.nvim",
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        pyright = {},
-      },
-    },
-  },
-  {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    branch = "main",
+    build = ":TSUpdate",
     opts = {
       ensure_installed = {
         "bash",
         "go",
         "html",
+        "java",
         "javascript",
         "json",
         "lua",
@@ -32,22 +22,170 @@ return {
         "typescript",
         "yaml",
       },
+      highlight = { enable = true },
+      indent = { enable = true },
+      fold = { enable = true },
     },
+    config = function(_, opts)
+      require("nvim-treesitter").setup(opts)
+    end,
   },
-  {"neovim/nvim-lspconfig", tag = "v1.8.0", pin = true},
-  {"hrsh7th/cmp-nvim-lsp"},
   {
-    "hrsh7th/nvim-cmp",
-    version = false,
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
+    "stevearc/conform.nvim",
+    dependencies = { "mason.nvim" },
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "black" },
+        go = { "goimports", "gofumpt" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      format_on_save = false,
     },
+    config = function(_, opts)
+      require("conform").setup(opts)
+      vim.keymap.set("", "<leader>CC", function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end, { desc = "Format code/selection" })
+    end,
   },
   {
     "folke/trouble.nvim",
     opts = { use_diagnostic_signs = true },
+  },
+  {
+    "mason-org/mason.nvim",
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    tag = "v1.8.0",
+    pin = true,
+    opts = {
+      servers = {
+        clangd = {},
+        gopls = {},
+        jdtls = {},
+        lua_ls = {},
+        marksman = {},
+        pyright = {},
+      },
+    },
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+    config = function(_, opts)
+      local lspconfig = require("lspconfig")
+      local blink = require("blink.cmp")
+
+      for server, config in pairs(opts.servers) do
+        config.capabilities = blink.get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    version = "1.*",
+    opts = {
+      keymap = {
+        preset = "default",
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide", "fallback" },
+        ["<C-y>"] = { "select_and_accept", "fallback" },
+
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+        ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+        ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+        ["<Tab>"] = { "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+        ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+      },
+      appearance = {
+        nerd_font_variant = "mono",
+      },
+      completion = { documentation = { auto_show = true } },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+    opts_extend = { "sources.default" },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    enabled = false,
+    version = false,
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-nvim-lsp",
+      "SirVer/ultisnips",
+    },
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.snippet.expand(args.body)
+            vim.fn["UltiSnips#Anon"](args.body)
+          end,
+        },
+
+        mapping = cmp.mapping.preset.insert({
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-CR>"] = function(fallback)
+            cmp.abort()
+            fallback()
+          end,
+        }),
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "ultisnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false },
+      })
+
+      vim.lsp.config("*", {
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      })
+    end,
   },
 }
---
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,114 @@
+return {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      delay = 500,
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = true })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
+  {
+    "stevearc/oil.nvim",
+    dependencies = { { "nvim-mini/mini.icons" } },
+    keys = {
+      { "-", "<Cmd>Oil<CR>", desc = "Browse files from here" },
+    },
+    opts = {
+      delete_to_trash = true,
+      view_options = {
+        show_hidden = true,
+      },
+    },
+    lazy = false,
+    config = function(_, opts)
+      require("oil").setup(opts)
+    end,
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    opts = {
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+      },
+      settings = {
+        save_on_toggle = true,
+      },
+    },
+    keys = function()
+      local harpoon = require("harpoon")
+      local keys = {
+        {
+          "<leader>H",
+          function()
+            harpoon:list():add()
+          end,
+          desc = "Harpoon Add File",
+        },
+        {
+          "<leader>h",
+          function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = "Harpoon Quick Menu",
+        },
+      }
+
+      for i = 1, 9 do
+        table.insert(keys, {
+          "<leader>h" .. i,
+          function()
+            harpoon:list():select(i)
+          end,
+          desc = "Harpoon to File " .. i,
+        })
+      end
+      return keys
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  },
+}
