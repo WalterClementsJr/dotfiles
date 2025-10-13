6a7480a319cc84019a090994ba095df89ab13d40
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2025-10-13 08:40:29 +0700

    chore: emacs update
    chore: emacs conf

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -1,6 +1,5 @@
-;;; -*- lexical-binding: t; -*-
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 (setq doom-theme 'doom-homage-black)
-(setq doom-font 'Jetbrains-Mono*)
 
 (setq display-line-numbers-type 'relative)
 (setq org-directory "~/org/")
