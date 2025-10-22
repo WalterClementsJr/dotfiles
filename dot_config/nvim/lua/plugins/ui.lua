25d82a64614ec40631e6ede0be99f87598754bc8
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2025-10-22 21:23:31 +0700

    feat: UI update

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -5,6 +5,19 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "16color",
+        section_separators = "",
+        component_separators = "",
+      },
+      sections = {
+        lualine_c = { { "filename", path = 1 } },
+      },
+    },
+    config = function(_, options)
+      require("lualine").setup(options)
+    end,
   },
   {
     "bluz71/vim-nightfly-colors",
